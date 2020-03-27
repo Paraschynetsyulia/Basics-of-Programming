9a3f3bde2b53b6e523b0091c0594b1af2ad8de36
Paraschynetsyulia <62383952+Paraschynetsyulia@users.noreply.github.com>
2020-03-27 11:55:31 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,17 +1,18 @@
-//завдання номер 1 , варіант 19
+//завдання номер 2 , варіант 19
 #include<iostream>
 using namespace std;
 int main() {
-    int n;
-    int a;
-    cout << "Input n=" << " ";
-    cin >> n;
-    while (n > 10) {
-        a = n / 10;
-        cout << a << " ";
-        n = (n - a) / 10;
- }
-    cout << n << " ";
+    double* a;
+    double* b;
+    a = new double;
+    b = new double;
+    cin >> *a;
+    cin >> *b;
+    int m = *a + *b;
+    cout << m << endl;
     system("pause");
     return 0;
+
+
+
 }
