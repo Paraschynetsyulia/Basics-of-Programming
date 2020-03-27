c226710002319f100937a73cb97f7bd6f5d54ac1
Paraschynetsyulia <62383952+Paraschynetsyulia@users.noreply.github.com>
2020-03-27 11:32:10 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+//завдання номер 2 , варіант 19
+#include<iostream>
+using namespace std;
+int main() {
+    double* a;
+    double* b;
+    a = new double;
+    b = new double;
+    cin >> *a;
+    cin >> *b;
+    int m = *a + *b;
+    cout << m << endl;
+    system("pause");
+    return 0;
+
+
+
+}
